e9501f4ff2ec73be46fd668f93b4d619f389f262
Assayyaad <hi@thealpha.pro>
2024-09-13 22:17:01 +0800

    refactor: تحسين كود أوامر التحميل

--- bin/download.js
+++ bin/download.js
@@ -1,87 +1,72 @@
 #!/usr/bin/env node
 
-import { existsSync, mkdirSync, writeFileSync, readFile } from 'fs'
-import { resolve, join } from 'path'
-import { get as httpGet } from 'http'
+import { existsSync, mkdirSync, readFile, writeFile } from 'fs'
 import { get as httpsGet } from 'https'
+import { get as httpGet } from 'http'
+import { join, resolve } from 'path'
 import { parse } from '../src/func/parse.js'
 
+/** @type {Config} */
 const config = {
-  /** @type {string | null} */
   url: null,
-  /** @type {string | null} */
   path: null,
-  /** @type {string | null} */
   file: null,
-  /** @type {string} */
   encoding: 'utf8'
 }
 
 process.argv.slice(2).forEach((arg) => {
   let [key, value] = arg.split('=')
   key = key.replace('--', '')
-  // eslint-disable-next-line no-prototype-builtins
-  if (config.hasOwnProperty(key)) {
-    config[key] = value
-  }
+  if (key in config) config[key] = value
 })
 
-if (!config.url && !config.path) {
-  console.error('Missing --url and --path argument, one of them is required')
-  process.exit(1)
-}
+const { url, path, file, encoding } = config
 
-if (!config.file) {
-  console.error('Missing --file argument')
-  process.exit(1)
-}
+if (!url && !path) throw new Error('Missing --url and --path argument, one of them is required')
+if (!file) throw new Error('Missing --file argument')
 
-if (config.url) {
-  const get = config.url.startsWith('https:') ? httpsGet : httpGet
-  get(config.url, (response) => {
+if (url) {
+  const get = url.startsWith('https:') ? httpsGet : httpGet
+  get(url, (res) => {
     let html = ''
-    response.on('data', (chunk) => (html += chunk))
-    response.on('end', () => writeFile(html, true))
-  }).on('error', (error) => {
-    console.error('Error loading HTML from URL:', error)
+    res.on('data', (chunk) => (html += chunk))
+    res.on('end', () => writeIt(html, true))
+  }).on('error', (err) => {
+    throw err
   })
-} else if (config.path) {
-  const filePath = resolve(config.path)
-  // @ts-expect-error
-  readFile(filePath, config.encoding, (error, html) => {
-    if (error) {
-      console.error('Error reading HTML file:', error)
-      return
-    }
-
-    writeFile(html)
+} else if (path) {
+  const filePath = resolve(path)
+  readFile(filePath, encoding, (err, html) => {
+    if (err) throw err
+    writeIt(html)
   })
 }
 
 /**
- *
  * @param {string} html .
- * @param {boolean} [withHtml] @default false
- * @returns {void}
+ * @param {boolean} [withHtml] Default is false.
+ * @returns {Promise<void>}
  */
-function writeFile(html, withHtml = false) {
-  if (!config.file) {
-    console.error('Missing --file argument')
-    process.exit(1)
-  }
+async function writeIt(html, withHtml = false) {
+  if (!file) throw new Error('Missing --file argument')
 
   const folderPath = join(process.cwd(), 'scrap')
-  if (!existsSync(folderPath)) {
-    mkdirSync(folderPath)
-  }
+  if (!existsSync(folderPath)) mkdirSync(folderPath)
 
   const json = JSON.stringify(parse(html), null, 2)
-  const filePath = join(folderPath, config.file)
+  const filePath = join(folderPath, file)
 
   // @ts-expect-error
-  writeFileSync(filePath + '.json', json, config.encoding)
-  if (withHtml) {
-    // @ts-expect-error
-    writeFileSync(filePath + '.html', html, config.encoding)
-  }
+  const promises = [writeFile(filePath + '.json', json, encoding)]
+  // @ts-expect-error
+  if (withHtml) promises.push(writeFile(filePath + '.html', html, encoding))
+  await Promise.all(promises)
 }
+
+/**
+ * @typedef {object} Config
+ * @property {string | null} url
+ * @property {string | null} path
+ * @property {string | null} file
+ * @property {BufferEncoding} encoding
+ */
