02f43833e5268db09ad1f2900babb902a1c99652
Assayyaad <hi@thealpha.pro>
2024-09-13 23:12:48 +0800

    fix: إستخراج دالة parse بشكل صحيح
    دالة parse أصبحت تستخرج من داخل الأداة بدل الإعتماد على طرد خارجي
    
    BREAKING CHANGE: لم يعد الطرد يعتمد على الطرد himalaya وإستبدله بنظام خاص داخلي

--- src/exports.js
+++ src/exports.js
@@ -1,5 +1,5 @@
 export * from './options.js'
 
-export { parse } from 'himalaya'
+export { parse } from './func/parse.js'
 export { findNode } from './func/find.js'
 export { grabNode, grabChild, grabAttr, grabText } from './func/grab.js'
